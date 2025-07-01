170c09cccea636ae5544ecf953111bf0ecc534d5
nickmassarelli <nickmass93@gmail.com>
2025-06-30 23:24:11 -0400

    nm

--- src/_data/slider.js
+++ src/_data/slider.js
@@ -215,4 +215,388 @@ module.exports = [
       alt: '',
     },
   },
+  // Image 19
+  {
+    caption: '',
+    mobileImage: {
+      src: '/assets/image-slider/horizontal-images/image-019_v.jpg',
+      alt: '',
+    },
+    desktopImage: {
+      src: '/assets/image-slider/horizontal-images/image-019.jpg',
+      alt: '',
+    },
+  },
+  // Image 20
+  {
+    caption: '',
+    mobileImage: {
+      src: '/assets/image-slider/horizontal-images/image-020.jpg',
+      alt: '',
+    },
+    desktopImage: {
+      src: '/assets/image-slider/horizontal-images/image-020.jpg',
+      alt: '',
+    },
+  },
+  // Image 21
+  {
+    caption: '',
+    mobileImage: {
+      src: '/assets/image-slider/horizontal-images/image-021.jpg',
+      alt: '',
+    },
+    desktopImage: {
+      src: '/assets/image-slider/horizontal-images/image-021.jpg',
+      alt: '',
+    },
+  },
+  // Image 22
+  {
+    caption: '',
+    mobileImage: {
+      src: '/assets/image-slider/horizontal-images/image-022.jpg',
+      alt: '',
+    },
+    desktopImage: {
+      src: '/assets/image-slider/horizontal-images/image-022.jpg',
+      alt: '',
+    },
+  },
+  // Image 23
+  {
+    caption: '',
+    mobileImage: {
+      src: '/assets/image-slider/horizontal-images/image-023.jpg',
+      alt: '',
+    },
+    desktopImage: {
+      src: '/assets/image-slider/horizontal-images/image-023.jpg',
+      alt: '',
+    },
+  },
+  // Image 24
+  {
+    caption: '',
+    mobileImage: {
+      src: '/assets/image-slider/horizontal-images/image-024_v.jpg',
+      alt: '',
+    },
+    desktopImage: {
+      src: '/assets/image-slider/horizontal-images/image-024.jpg',
+      alt: '',
+    },
+  },
+  // Image 25
+  {
+    caption: '',
+    mobileImage: {
+      src: '/assets/image-slider/horizontal-images/image-025.jpg',
+      alt: '',
+    },
+    desktopImage: {
+      src: '/assets/image-slider/horizontal-images/image-025.jpg',
+      alt: '',
+    },
+  },
+  // Image 26
+  {
+    caption: '',
+    mobileImage: {
+      src: '/assets/image-slider/horizontal-images/image-026.jpg',
+      alt: '',
+    },
+    desktopImage: {
+      src: '/assets/image-slider/horizontal-images/image-026.jpg',
+      alt: '',
+    },
+  },
+  // Image 27
+  {
+    caption: '',
+    mobileImage: {
+      src: '/assets/image-slider/horizontal-images/image-027_v.jpg',
+      alt: '',
+    },
+    desktopImage: {
+      src: '/assets/image-slider/horizontal-images/image-027.jpg',
+      alt: '',
+    },
+  },
+  // Image 28
+  {
+    caption: '',
+    mobileImage: {
+      src: '/assets/image-slider/horizontal-images/image-028.jpg',
+      alt: '',
+    },
+    desktopImage: {
+      src: '/assets/image-slider/horizontal-images/image-028.jpg',
+      alt: '',
+    },
+  },
+  // Image 29
+  {
+    caption: '',
+    mobileImage: {
+      src: '/assets/image-slider/horizontal-images/image-029_v.jpg',
+      alt: '',
+    },
+    desktopImage: {
+      src: '/assets/image-slider/horizontal-images/image-029.jpg',
+      alt: '',
+    },
+  },
+  // Image 30
+  {
+    caption: '',
+    mobileImage: {
+      src: '/assets/image-slider/horizontal-images/image-030.jpg',
+      alt: '',
+    },
+    desktopImage: {
+      src: '/assets/image-slider/horizontal-images/image-030.jpg',
+      alt: '',
+    },
+  },
+  // Image 31
+  {
+    caption: '',
+    mobileImage: {
+      src: '/assets/image-slider/horizontal-images/image-031_v.jpg',
+      alt: '',
+    },
+    desktopImage: {
+      src: '/assets/image-slider/horizontal-images/image-031.jpg',
+      alt: '',
+    },
+  },
+  // Image 32
+  {
+    caption: '',
+    mobileImage: {
+      src: '/assets/image-slider/horizontal-images/image-032.jpg',
+      alt: '',
+    },
+    desktopImage: {
+      src: '/assets/image-slider/horizontal-images/image-032.jpg',
+      alt: '',
+    },
+  },
+  // Image 33
+  {
+    caption: '',
+    mobileImage: {
+      src: '/assets/image-slider/horizontal-images/image-033.jpg',
+      alt: '',
+    },
+    desktopImage: {
+      src: '/assets/image-slider/horizontal-images/image-033.jpg',
+      alt: '',
+    },
+  },
+  // Image 34
+  {
+    caption: '',
+    mobileImage: {
+      src: '/assets/image-slider/horizontal-images/image-034.jpg',
+      alt: '',
+    },
+    desktopImage: {
+      src: '/assets/image-slider/horizontal-images/image-034.jpg',
+      alt: '',
+    },
+  },
+  // Image 35
+  {
+    caption: '',
+    mobileImage: {
+      src: '/assets/image-slider/horizontal-images/image-035.jpg',
+      alt: '',
+    },
+    desktopImage: {
+      src: '/assets/image-slider/horizontal-images/image-035.jpg',
+      alt: '',
+    },
+  },
+  // Image 36
+  {
+    caption: '',
+    mobileImage: {
+      src: '/assets/image-slider/horizontal-images/image-036.jpg',
+      alt: '',
+    },
+    desktopImage: {
+      src: '/assets/image-slider/horizontal-images/image-036.jpg',
+      alt: '',
+    },
+  },
+  // Image 37
+  {
+    caption: '',
+    mobileImage: {
+      src: '/assets/image-slider/horizontal-images/image-037.jpg',
+      alt: '',
+    },
+    desktopImage: {
+      src: '/assets/image-slider/horizontal-images/image-037.jpg',
+      alt: '',
+    },
+  },
+  // Image 38
+  {
+    caption: '',
+    mobileImage: {
+      src: '/assets/image-slider/horizontal-images/image-038.jpg',
+      alt: '',
+    },
+    desktopImage: {
+      src: '/assets/image-slider/horizontal-images/image-038.jpg',
+      alt: '',
+    },
+  },
+  // Image 39
+  {
+    caption: '',
+    mobileImage: {
+      src: '/assets/image-slider/horizontal-images/image-039.jpg',
+      alt: '',
+    },
+    desktopImage: {
+      src: '/assets/image-slider/horizontal-images/image-039.jpg',
+      alt: '',
+    },
+  },
+  // Image 40
+  {
+    caption: '',
+    mobileImage: {
+      src: '/assets/image-slider/horizontal-images/image-040.jpg',
+      alt: '',
+    },
+    desktopImage: {
+      src: '/assets/image-slider/horizontal-images/image-040.jpg',
+      alt: '',
+    },
+  },
+  // Image 41
+  {
+    caption: '',
+    mobileImage: {
+      src: '/assets/image-slider/horizontal-images/image-041.jpg',
+      alt: '',
+    },
+    desktopImage: {
+      src: '/assets/image-slider/horizontal-images/image-041.jpg',
+      alt: '',
+    },
+  },
+  // Image 42
+  {
+    caption: '',
+    mobileImage: {
+      src: '/assets/image-slider/horizontal-images/image-042.jpg',
+      alt: '',
+    },
+    desktopImage: {
+      src: '/assets/image-slider/horizontal-images/image-042.jpg',
+      alt: '',
+    },
+  },
+  // Image 43
+  {
+    caption: '',
+    mobileImage: {
+      src: '/assets/image-slider/horizontal-images/image-043.jpg',
+      alt: '',
+    },
+    desktopImage: {
+      src: '/assets/image-slider/horizontal-images/image-043.jpg',
+      alt: '',
+    },
+  },
+  // Image 44
+  {
+    caption: '',
+    mobileImage: {
+      src: '/assets/image-slider/horizontal-images/image-044.jpg',
+      alt: '',
+    },
+    desktopImage: {
+      src: '/assets/image-slider/horizontal-images/image-044.jpg',
+      alt: '',
+    },
+  },
+  // Image 45
+  {
+    caption: '',
+    mobileImage: {
+      src: '/assets/image-slider/horizontal-images/image-045.jpg',
+      alt: '',
+    },
+    desktopImage: {
+      src: '/assets/image-slider/horizontal-images/image-045.jpg',
+      alt: '',
+    },
+  },
+  // Image 46
+  {
+    caption: '',
+    mobileImage: {
+      src: '/assets/image-slider/horizontal-images/image-046.jpg',
+      alt: '',
+    },
+    desktopImage: {
+      src: '/assets/image-slider/horizontal-images/image-046.jpg',
+      alt: '',
+    },
+  },
+  // Image 47
+  {
+    caption: '',
+    mobileImage: {
+      src: '/assets/image-slider/horizontal-images/image-047.jpg',
+      alt: '',
+    },
+    desktopImage: {
+      src: '/assets/image-slider/horizontal-images/image-047.jpg',
+      alt: '',
+    },
+  },
+  // Image 48
+  {
+    caption: '',
+    mobileImage: {
+      src: '/assets/image-slider/horizontal-images/image-048.jpg',
+      alt: '',
+    },
+    desktopImage: {
+      src: '/assets/image-slider/horizontal-images/image-048.jpg',
+      alt: '',
+    },
+  },
+  // Image 49
+  {
+    caption: '',
+    mobileImage: {
+      src: '/assets/image-slider/horizontal-images/image-049_v.jpg',
+      alt: '',
+    },
+    desktopImage: {
+      src: '/assets/image-slider/horizontal-images/image-049.jpg',
+      alt: '',
+    },
+  },
+  // Image 50
+  {
+    caption: '',
+    mobileImage: {
+      src: '/assets/image-slider/horizontal-images/image-050_v.jpg',
+      alt: '',
+    },
+    desktopImage: {
+      src: '/assets/image-slider/horizontal-images/image-050.jpg',
+      alt: '',
+    },
+  },
 ]
